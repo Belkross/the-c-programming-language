42c1378d883b4a16fad0150dd4ac12e11fc0aba5
belkross <belkross.pro@gmail.com>
2024-04-08 11:57:53 +0200

    refactor: replace magic numbers with symbolic constants

--- src/chapter-1/1-5.c
+++ src/chapter-1/1-5.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 
+#define MIN_VALUE 0
+#define MAX_VALUE 300
+#define INTERVAL 20
+
 float	convertFarenheitToCelsius(float farenheit)
 {
 	return (5.0 / 9.0) * (farenheit - 32.0);
@@ -8,7 +12,8 @@ float	convertFarenheitToCelsius(float farenheit)
 int	main(void)
 {
 	printf("\nFarenheit - Celsius\n");
-	for (int farenheit = 300; farenheit >= 0; farenheit = farenheit - 20)
+	for (int farenheit = MAX_VALUE; farenheit >= MIN_VALUE; farenheit = farenheit
+		- INTERVAL)
 	{
 		printf("%3d %5.1f\n", farenheit, convertFarenheitToCelsius(farenheit));
 	}
